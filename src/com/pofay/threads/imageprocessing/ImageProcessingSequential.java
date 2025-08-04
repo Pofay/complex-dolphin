e27b4ab5e848f87245b1f2e4bd7474757a3474c9
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-08-04 13:42:22 +0800

    WIP

---
+++ src/com/pofay/threads/imageprocessing/ImageProcessingSequential.java
@@ -0,0 +1,37 @@
+package com.pofay.threads.imageprocessing;
+
+public class ImageProcessingSequential {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int getBlue(int rgb) {
+        return rgb & 0x000000FF;
+    }
+
+    public static int getGreen(int rgb) {
+        return (rgb & 0x0000FF00) >> 8;
+    }
+
+    public static int getRed(int rgb) {
+        return (rgb & 0x00FF0000) >> 16;
+    }
+
+    public static int createRGBFromColors(int red, int green, int blue) {
+        int rgb = 0;
+
+        rgb |= blue;
+        rgb |= green << 8;
+        rgb |= red << 16;
+
+        rgb |= 0xFF000000;
+
+        return rgb;
+    }
+
+    public static boolean isShadeOfGrey(int red, int green, int blue) {
+        return Math.abs(red - green) < 30 && Math.abs(red - blue) < 30 && Math.abs(green - blue) < 30;
+    }
+
+}
