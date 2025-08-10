1b22da0528ad484c9590542e64c1b0ad4d81878c
Pofay <gianCarlo.gilos@gmail.com>
2025-08-10 10:08:13 +0800

    Completed Sequential version of Image Processing example

--- src/com/pofay/threads/imageprocessing/ImageProcessingSequential.java
+++ src/com/pofay/threads/imageprocessing/ImageProcessingSequential.java
@@ -1,9 +1,71 @@
 package com.pofay.threads.imageprocessing;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class ImageProcessingSequential {
 
-    public static void main(String[] args) {
+    public static final String SOURCE_FILE = "./resources/many-flowers.jpg";
+    public static final String DESTINATION_FILE = "./out/many-flowers.jpg";
 
+    public static void main(String[] args) throws IOException {
+
+        final var originalImage = ImageIO.read(new File(SOURCE_FILE));
+        final var resultImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(),
+                BufferedImage.TYPE_INT_RGB);
+
+        recolorSingleThreaded(originalImage, resultImage);
+
+        final var outputFile = new File(DESTINATION_FILE);
+        ImageIO.write(resultImage, "jpg", outputFile);
+    }
+
+    public static void recolorSingleThreaded(BufferedImage original, BufferedImage result) {
+        recolorImage(original, result, 0, 0, original.getWidth(), original.getHeight());
+    }
+
+    public static void recolorImage(BufferedImage original, BufferedImage result, int leftCorner, int topCorner,
+            int width, int height) {
+
+        for (int x = leftCorner; x < leftCorner + width && x < original.getWidth(); x++) {
+            for (int y = topCorner; y < topCorner + height && y < original.getWidth(); y++) {
+                recolorPixel(original, result, x, y);
+            }
+        }
+
+    }
+
+    public static void recolorPixel(BufferedImage original, BufferedImage result, int x, int y) {
+        int rgb = original.getRGB(x, y);
+
+        int red = getRed(rgb);
+        int green = getGreen(rgb);
+        int blue = getBlue(rgb);
+
+        int newRed;
+        int newGreen;
+        int newBlue;
+
+        if (isShadeOfGrey(red, green, blue)) {
+            newRed = Math.min(255, red + 10);
+            newGreen = Math.max(0, green - 80);
+            newBlue = Math.max(0, blue - 20);
+        } else {
+            newRed = red;
+            newGreen = green;
+            newBlue = blue;
+        }
+
+        int newRGB = createRGBFromColors(newRed, newGreen, newBlue);
+        setRGB(result, x, y, newRGB);
+    }
+
+    public static void setRGB(BufferedImage image, int x, int y, int rgb) {
+        final var rgbModel = image.getColorModel().getDataElements(rgb, null);
+        image.getRaster().setDataElements(x, y, rgbModel);
     }
 
     public static int getBlue(int rgb) {
