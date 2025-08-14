37a714c3ebd59b00625e634ee37fb7d70c93dd15
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-08-14 10:52:18 +0800

    chore: Cleanup unused packages

--- src/com/pofay/threads/exercises/filedownload/SingleThreadedFileDownload.java
+++ src/com/pofay/threads/exercises/filedownload/SingleThreadedFileDownload.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 
 public class SingleThreadedFileDownload {
 
