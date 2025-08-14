2f67e59fcb84821e54d9e361aa66c0754bfe2698
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-08-14 10:51:41 +0800

    wip: Added download capability to single threaded version of file downloader

--- src/com/pofay/threads/exercises/filedownload/SingleThreadedFileDownload.java
+++ src/com/pofay/threads/exercises/filedownload/SingleThreadedFileDownload.java
@@ -1,5 +1,7 @@
 package com.pofay.threads.exercises.filedownload;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
@@ -9,11 +11,12 @@ import java.net.URL;
 public class SingleThreadedFileDownload {
 
     // Main Download logic is from https://www.baeldung.com/java-download-file
-    private static String FILE_URL = "https://drive.google.com/file/d/13QoKVFLz_SjSRXhuieXg8EnZn2bs_wNL/view?usp=drive_link";
-    
+    private static String FILE_URL = "https://cdn.britannica.com/21/75121-050-8CF5E1DB/Bats-structures-organs-sound-frequencies-signals-contexts.jpg";
+    private static String FILE_NAME = "./resources/Bat.jpg";
+
     public static void main(String[] args) throws URISyntaxException, IOException {
 
-        // For now print out the file size 
+        // For now print out the file size
         final var url = new URI(FILE_URL).toURL();
         final var httpConnection = (HttpURLConnection) url.openConnection();
         httpConnection.setRequestMethod("HEAD");
@@ -23,6 +26,16 @@ public class SingleThreadedFileDownload {
         System.out.println("File size is: " + fileSize);
 
         // Then download the file
+        try (final var urlInputStream = new BufferedInputStream(url.openStream());
+                FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
+            byte dataBuffer[] = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = urlInputStream.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            // handle exception
+        }
 
     }
 
